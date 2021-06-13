908b063ff6b06f12e8efd3af4519dea2d343202c
Dikshitha Rao <dikshitharao48798@gmail.com>
2021-06-13 17:34:09 +0530

    Add files via upload
    A code to show all the different variables and their working and how they can be accessed in java.

---
+++ VariablesExample/src/com/blogging/variables/Demo.java
@@ -0,0 +1,11 @@
+package com.blogging.variables;
+
+public class Demo {
+	int id;//Instance variable
+	String name;//Instance variable
+	static int num = 100;//static variable
+	final int f = 23;//Final variable
+	
+	//Observe the ClassA class compare it with this Demo class.
+
+}
